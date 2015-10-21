10998a24e86c219ce91b6c1ba7c2eadb86702fbd
Kyle Gorak <kyle@gorak.us>
2015-10-21 11:19:54 -0400

    added error handling

--- server_con/api_cons.py
+++ server_con/api_cons.py
@@ -1,7 +1,7 @@
-import config
-import requests # connect to server
+import config # api keys
 from bs4 import BeautifulSoup # parse XML response
-import pycurl # send post request
+import pycurl # send requests
+from urllib.parse import urlencode # encode parameters for get request
 from io import BytesIO # read post response 
 from os.path import realpath # get absolute path for image upload TODO:// remove, use cat API
 
@@ -9,7 +9,7 @@ class SendSpace(object):
   # class variables
   sendspace_url = 'http://api.sendspace.com/rest/'
   api_key = config.key
-
+  image_data = {}
   def __init__(self, file):
     self.file = file
   
@@ -17,15 +17,27 @@ class SendSpace(object):
   ### Connect to SendSpace as anonymous user
   ###
   def connect(self):
-    connect_data = {'method':'anonymous.uploadGetInfo', 'api_key':self.api_key, 'api_version':1.0}
+    # parameters for anonymous upload info
+    connect_params = {'method':'anonymous.uploadGetInfo', 'api_key':self.api_key, 'api_version':1.0}
+    
     # get request to get info for anonymous upload
-    con_r = requests.get(self.sendspace_url, params=connect_data)
-    # error handling
-    if con_r.status_code != 200:
-    	# not OK connection
-    	print('Status: ', con_r.status_code, 'Problem with the request. Exiting.')
-    	exit()
-    else: return con_r
+    # TODO:// error handling
+    c = pycurl.Curl()
+    c.setopt(c.URL, self.sendspace_url + '?' + urlencode(connect_params))
+    c.setopt(c.HTTPGET, 1)
+    b = BytesIO()
+    c.setopt(pycurl.WRITEDATA, b)
+    c.perform()
+    response_code = c.getinfo(pycurl.HTTP_CODE)
+    if (response_code != 200): 
+      print("Error getting info for upload: " + response_code)
+      c.close()
+      exit()
+    c.close()
+
+    con_r = b.getvalue().decode('utf-8')
+    return con_r
+  
   ###
   ### parse response as xml
   ###
@@ -36,47 +48,59 @@ class SendSpace(object):
   ### Send file for upload
   ###
   def uploadImage(self, xml_data):
-    # prep image for upload
-
+    # initialize post request parameter values or exit
+    try:
+      upl_url = xml_data.result.upload["url"]
+      upl_progress_url = xml_data.result.upload["progress_url"]
+      upl_max_file_size = int(xml_data.result.upload["max_file_size"])
+      upl_upload_identifier = xml_data.result.upload["upload_identifier"]
+      upl_extra_info = xml_data.result.upload["extra_info"]
+      #post_data['MAX_FILE_SIZE'] = con_max_file_size # already in url
+      #post_data['UPLOAD_IDENTIFIER'] = con_upload_identifier # already in url
+    except Exception as e:
+        try: 
+          print("Error parsing info for upload: " + str(xml_data.body.error))
+          exit()
+        except Exception as e2:
+          print("Error: " + str(e) + ' ' + str(e2))
+          exit()
     # TODO:// generate image from cat API
     filename = realpath('duca2.jpg')
-
-    # prep xml post request 
-    #connection response: url=[upload url], progress_url=[real-time progress information]
-    #                     max_file_size=[max size curent user can upload], upload_identifier=[], extra_info=[]
-    upl_url = xml_data["url"]
-    upl_progress_url = xml_data["progress_url"]
-    upl_max_file_size = int(xml_data["max_file_size"])
-    upl_upload_identifier = xml_data["upload_identifier"]
-    upl_extra_info = xml_data["extra_info"]
-    #post_data['MAX_FILE_SIZE'] = con_max_file_size # already in url
-    #post_data['UPLOAD_IDENTIFIER'] = con_upload_identifier # already in url
-    post_data = [
+    
+    # parameters for anonymous image upload
+    post_params = [
       ('extra_info', upl_extra_info),
       ('userfile', (pycurl.FORM_FILE, filename,)),
     ]
 
-
+    # post request to upload image
     c = pycurl.Curl()
     c.setopt(c.URL, upl_url)
-    c.setopt(c.HTTPPOST, post_data)
+    c.setopt(c.HTTPPOST, post_params)
     b = BytesIO()
     c.setopt(pycurl.WRITEDATA, b)
     c.perform()
     c.close()
 
-    post_r = b.getvalue().decode('utf-8')
-
-    # post response: download_url, delete_url, file_id
-    return post_r
+    return b.getvalue().decode('utf-8')
 
 sendSpace = SendSpace("")
-con_r = sendSpace.connect()
-parsed_con_r = sendSpace.parseXML(con_r.text).result.upload
-upl_r = sendSpace.uploadImage(parsed_con_r)
-parsed_upl_r = sendSpace.parseXML(upl_r)
+try:
+  con_r = sendSpace.connect()
+  parsed_con_r = sendSpace.parseXML(con_r)
+  upl_r = sendSpace.uploadImage(parsed_con_r)
+  parsed_upl_r = sendSpace.parseXML(upl_r)
 
-download_url = parsed_upl_r.download_url.string
-delete_url = parsed_upl_r.delete_url.string
-
-print(download_url)
+  try:
+    sendSpace.image_data['download_url'] = parsed_upl_r.download_url.string
+    sendSpace.image_data['delete_url'] = parsed_upl_r.delete_url.string
+  except Exception as e:
+    try: 
+      print("Error parsing info for upload: " + str(parsed_upl_r.body))
+      exit()
+    except Exception as e2:
+      print("Error: " + str(e) + ' ' + str(e2))
+      exit()
+  print(sendSpace.image_data['download_url'])
+except Exception as e: 
+  print("Cannot upload at this time: " + str(e))
