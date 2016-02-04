67013172b9d74d215edb407687ade194c4e5ac25
Kyle Gorak <kyle@gorak.us>
2016-02-04 11:32:29 -0500

    created setup file

---
+++ setup.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+from setuptools import setup, find_packages
+
+with open('README.rst', 'r') as f:
+    readme = f.read()
+
+setup(
+    name='covertFS',
+    version='0.7.0',
+    description='It\'s covert and stuff',
+    long_description=readme,
+    author='Kyle Gorak, David Hart, Adam Sjoholm, and Ryne Flores',
+    author_email='kyle.gorak@usma.edu',
+    url='https://github.com/gorhack/covertFS',
+    license='USMA',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: File System',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+    ],
+    keywords='covert file system steganography',
+    packages=find_packages(exclude=['docs']),
+    install_requires=[
+        "requests",
+        "beautifulsoup4",
+        "fs",
+        "lxml",
+        "Pillow",
+        "fusepy",
+    ]
+)
