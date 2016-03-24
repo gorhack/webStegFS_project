3cf0b3ae62e868a5c3b4e569cff63b654c9427e8
Kyle Gorak <kyle@gorak.us>
2016-03-24 14:01:43 -0400

    updated distributions

--- setup.py
+++ setup.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+
 from setuptools import setup, find_packages
 
 with open('README.rst', 'r') as f:
@@ -6,7 +7,7 @@ with open('README.rst', 'r') as f:
 
 setup(
     name='covertFS',
-    version='0.9.1',
+    version='0.9.2rc1',
     description='It\'s covert and stuff',
     long_description=readme,
     author='Kyle Gorak, David Hart, Adam Sjoholm, and Ryne Flores',
@@ -14,7 +15,7 @@ setup(
     url='https://github.com/gorhack/covertFS',
     license='USMA',
     classifiers=[
-        'Development Status :: 3 - Alpha',
+        'Development Status :: 4 - Beta',
         'Intended Audience :: Developers',
         'Topic :: Software Development :: File System',
         'Programming Language :: Python :: 3',
@@ -23,7 +24,11 @@ setup(
         'Operating System :: POSIX',
     ],
     keywords='covert file system steganography',
-    packages=find_packages(exclude=['docs']),
+    packages=find_packages(exclude=['docs', 'dist', 'design', 'paper',
+                           'covertFS.egg-info', '__pycache__', '.idea']),
+    package_data={
+        'README': ['README.rst'],
+    },
     install_requires=[
         "requests",
         "beautifulsoup4",
